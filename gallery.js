5aa0fb1ddbb0459109526905fcf6685f093087bb
Noelle Jade <dualfighter1@gmail.com>
2024-10-15 15:41:55 +0100

    Odd fixes

--- gallery.js
+++ gallery.js
@@ -1,24 +1,16 @@
-// Get the elements with class="column"
 var elements = document.getElementsByClassName("column");
 
 for (var i = 0; i < elements.length; i++) {
   elements[i].style.flex = "20%";
 }
 
-// Modal
-// create references to the modal...
 var modal = document.getElementById('modalID');
-// to all images -- note I'm using a class!
 var images = document.getElementsByClassName('modalImages');
-// the image in the modal
 var modalImg = document.getElementById("img01");
-// and the caption in the modal
 var captionText = document.getElementById("caption");
 
-// Go through all of the images with our custom class
 for (var i = 0; i < images.length; i++) {
   var img = images[i];
-  // and attach our click listener for this image.
   img.onclick = function(evt) {
     modal.style.display = "block";
     modalImg.src = this.src;
@@ -27,7 +19,6 @@ for (var i = 0; i < images.length; i++) {
 }
 
 var span = document.getElementsByClassName("close")[0];
-
 span.onclick = function() {
   modal.style.display = "none";
 }
